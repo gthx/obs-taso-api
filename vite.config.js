89a3311c1d6a0c47619c19a1cb0a3b7f8d5f6124
Tuomas Harju <harju.tuomas.juhani@gmail.com>
2025-08-13 14:06:05 +0300

    update base config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/obs-taso-api/",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
-        overlay: resolve(__dirname, 'overlay.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin.html"),
+        overlay: resolve(__dirname, "overlay.html"),
+      },
+    },
+  },
+});
