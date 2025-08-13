ae19a593f1560f275c7a265a2caa5316dc032dd2
Tuomas Harju <harju.tuomas.juhani@gmail.com>
2025-08-13 14:17:20 +0300

    fix absolute URLs

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <main>
     <div>
-        <a href="/overlay.html" target="_blank" rel="noreferrer">Overlay</a>
-        <a href="/admin.html" target="_blank" rel="noreferrer">Admin</a>
+        <a href="overlay.html" target="_blank" rel="noreferrer">Overlay</a>
+        <a href="admin.html" target="_blank" rel="noreferrer">Admin</a>
     </div>
 </main>
